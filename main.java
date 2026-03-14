8e61cdc80a762769dcbe5eace68546a521330043
Manikonda Nikhila <nikhilamanikonda@gmail.com>
2026-03-14 21:30:33 +0530

    Added code to main.java

--- main.java
+++ main.java
@@ -1,2 +1,5 @@
-System.out.println("Hello from main");
-System.out.println("Hello from feature branch");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Skill 1");
+    }
+}
